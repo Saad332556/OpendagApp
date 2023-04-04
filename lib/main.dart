5eca6c22c1402f786c52b61953db86bac0e605f8
Saad332556 <332556@student.mboutrecht.nl>
2023-04-04 14:07:54 +0200

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-}
-
-class _MyHomePage {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Opendag app',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Opendag app'),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 70),
+              Text(
+                'Welkom',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Test test test',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    child: Image.asset(
+                      'images/',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    child: Image.asset(
+                      'images/',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
